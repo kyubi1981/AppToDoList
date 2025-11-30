035444fd155bc046405f0f0f62cc428891ce43ac
Antonio Rodriguez Rodriguez <rodriguezdomingoantonio@gmail.com>
2025-11-30 03:00:20 -0300

    Desafio5 - Metodos de Arreglos - ToDoList, avance al 85%

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@ const tareas = [
     {
         id: 3,
         task: "lavar platos",
-        estado: false
+        estado: true
     },
     {
         id: 4,
@@ -23,31 +23,6 @@ const tareas = [
     {
         id: 5,
         task: "peinar la muñeca",
-        estado: false
-    },
-    {
-        id: 6,
-        task: "bañar perrito",
-        estado: false
-    },
-    {
-        id: 7,
-        task: "comer",
-        estado: false
-    },
-    {
-        id: 8,
-        task: "peinar la muñeca",
-        estado: false
-    },
-    {
-        id: 9,
-        task: "bañar perrito",
-        estado: false
-    },
-    {
-        id: 10,
-        task: "comer",
         estado: false
     }
 
@@ -60,55 +35,106 @@ function llenaTabla() {
     const taskRealizadas = document.getElementById("txtTaskRealizadas");
 
     let tabla = "";
-    /* let totTareas = ""; */
-    let okTareas = "";
+    let okTareas = 0;
 
     tabla +=
-        `
-    <tr>
-        <th width="30px;">ID</th>
-        <th width="120px;">Tarea</th>
-        <th width="20px;">**</th>
-        <th width="20px;">**</th>
-    </tr>
-    `;
+        `<thead>
+            <tr>
+                <th width="30px;">ID</th>
+                <th width="120px;">Tarea</th>
+                <th width="20px;">**</th>
+                <th width="20px;">**</th>
+            </tr>
+        </thead>
+        `;
 
     for (const tarea of tareas) {
-        tabla += /* `${tarea.task}`; */
-            `<tr>    
-            <td style="text-align: center;">${tarea.id}</td>
-             <td style="text-align: left;">${tarea.task}</td>
-             <td style="text-align: center;"><input onchange="functOkTask();" type="checkbox" id="chk-okey" name="opcion" value="ejemplo"></td>
-             <td style="text-align: center;"><button onclick="functDelTask();" class="material-symbols-outlined" ="#EA3323">delete</button></td>
-        </tr>  
+        tabla +=
+            `<tbody >
+            <tr>    
+                <td style="text-align: center;">${tarea.id}</td>
+                <td style="text-align: left;">${tarea.task}</td>
+                <td style="text-align: center;"><input onchange="functOkTask(${tarea.id});" type="checkbox" id="chk-okey" name="opcion" value=""></td>
+                <td style="text-align: center;"><button onclick="functDelTask(${tarea.id});" class="material-symbols-outlined" style="color: red;">delete</button></td>
+            </tr>  
+        </tbody>
         `;
+
+        if (`${tarea.estado}` === true) {
+            okTareas += 1;
+            alert(okTareas);
+        }
     }
     listaTareas.innerHTML = tabla;
     taskTotales.innerHTML = `${tareas.length}`;
+
+    //alert("tareas listas... " + Number(okTareas));
+
+    taskRealizadas.innerHTML = Number(okTareas);
 
 
     //console.log(tabla);
     //console.log(tareas.length);
 }
 
-
 function functAddTask() {
-    alert("tarea agregada...")
+    const agregaTarea = document.getElementById("txt-tarea");
+
+    let ultimo = "";
+
+    for (const ultimoId of tareas) {
+        ultimo = `${ultimoId.id}`;
+    }
+
+    ultimo = Number(ultimo) + 1;
+
+    //alert("el ultimo id es " + ultimo);
+
+    const nuevoObjetoTarea = {
+        id: ultimo,
+        task: agregaTarea.value,
+        estado: false
+    }
+    tareas.push(nuevoObjetoTarea);
+
+    agregaTarea.value = "";
+
+    llenaTabla();
 }
 
 function functDelTask(key) {
-    alert("tarea eliminada...");
+
+    //alert("tarea eliminada..." + key);
+
+    const pos = tareas.findIndex((tarea) => tarea.id === key);
+
+    //alert("posicion es... " + pos);
+
+    tareas.splice(pos, 1);
+
+    llenaTabla();
+
 }
 
-function functOkTask() {
-    alert("tarea terminada");
+function functOkTask(dato) {
+
+    alert("tarea terminada " + dato);
+
+    for (const tarea of tareas) {
+
+        if (`${tarea.estado}` === true) {
+            okTareas += 1;
+        }
+    }
+
+    const pos = tareas.findIndex((tarea) => tarea.id === key);
+
+    //alert("posicion es... " + pos);
+
+    tareas.splice(pos, 1);
+
+    llenaTabla();
 }
 
-/*
-const agregaTarea = function () {
-    const txtNuevaTarea = document.querySelector('#');
-}
-*/
 
 llenaTabla();
-
